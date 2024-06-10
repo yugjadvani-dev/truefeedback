d2cc1267dcf26f40e49df9c4f22e5a1023b4fb87
Yug Jadvani <yugjadvani.dev@gmail.com>
2024-06-10 22:34:18 +0530

    "Updated GET function parameter type from Response to NextRequest"

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -58,7 +58,7 @@ export async function POST(request: NextRequest) {
   }
 }
 
-export async function GET(request: Response) {
+export async function GET(request: NextRequest) {
   // Connect to the database
   await dbConnect();
 
